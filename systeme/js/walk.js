c0d1ec66817787ca900289172178c12b67131f6a
collectif11880 <pascalrollin292@gmail.com>
2025-08-03 10:53:32 +0200

    modifier les valeurs de la zonne de déplacement de l'avatar. fait un teste de présence dans la zonne : tout fonctionne

--- systeme/js/walk.js
+++ systeme/js/walk.js
@@ -1,6 +1,6 @@
 // jQuery(document).ready(function($){
-	const fin_recul = 400; // position mini de l'avatar
-	const fin_avanc = 750; //position max de l'avatar 
+	const fin_recul = 300; // position mini de l'avatar
+	const fin_avanc = 900; //position max de l'avatar 
 	let fin_decor = -3500; // position max du decor
 	let deb_decor = 0; // position de debut du decor
 	let avc_monde = 0; // nombre de pixel d'avance du monde
